966d0af9a5fc186a7e1f0cd787a912d16fb469da
thitikorn_nupan@outlook.co.th <thitikorn_nupan@outlook.co.th>
2025-12-02 19:07:53 +0700

    commit

--- src/test/java/com/ttknp/basicapi/BasicApiUsingSpringBootApplicationTests.java
+++ src/test/java/com/ttknp/basicapi/BasicApiUsingSpringBootApplicationTests.java
@@ -1,13 +1,34 @@
 package com.ttknp.basicapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+// ** Bug test single method on spring boot 4
+
+// *** JUnit5 test cases for CRUD REST APIs. use the @WebMvcTest annotation to load only UserController class. (can multiple rest controller)
+// @WebMvcTest(PhoneController.class)
+// Or
+// JUnit6
 @SpringBootTest
+@AutoConfigureMockMvc
 class BasicApiUsingSpringBootApplicationTests {
 
+    // *** using MockMvc class to make REST API calls.
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    public void testSelectAll() throws Exception {
+        mockMvc.perform(get("/api/phone/reads"))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
+
 
 }
---
+++ src/test/java/com/ttknp/basicapi/BasicApiUsingSpringBootServiceTests.java
@@ -0,0 +1,26 @@
+package com.ttknp.basicapi;
+
+import com.ttknp.basicapi.service.PhoneService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+// ** Bug test single method on spring boot 4
+
+// In junit 6 @SpringBootTest it's no need
+@SpringBootTest // in junit 6 if you cut @SpringBootTest out will not run main
+class BasicApiUsingSpringBootServiceTests {
+
+    private final PhoneService phoneService = new PhoneService();
+
+    @Test
+    void contextLoads() {
+        assertThat(phoneService).isNotNull();
+    }
+
+    @Test
+    public void testGetAll() {
+       assertThat(phoneService.getPhones().size()).isEqualTo(3);
+    }
+
+}
