db7ccd27705af2a4b63f66ae74308b0e6747d2a5
thitikorn_nupan@outlook.co.th <thitikorn_nupan@outlook.co.th>
2025-12-07 17:48:42 +0700

    commit

--- src/main/java/com/ttknp/BasicApiUsingSpringBoot3Application.java
+++ src/main/java/com/ttknp/BasicApiUsingSpringBoot3Application.java
@@ -1,13 +1,19 @@
 package com.ttknp;
 
+import com.ttknp.valiadationcustomservice.validation.ValidateHelperService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+// @ComponentScan(basePackages = {"com.ttknp"}) // *** if you gonna test (with mockito) you have to comment this annotation
+@Slf4j
+@SpringBootApplication(scanBasePackages = "com.ttknp")
 public class BasicApiUsingSpringBoot3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(BasicApiUsingSpringBoot3Application.class, args);
+        log.info("Application started {}", ValidateHelperService.isEmail("thitikorn_nupan@gmail.com"));
     }
 
 }
